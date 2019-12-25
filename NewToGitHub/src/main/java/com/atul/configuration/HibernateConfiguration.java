43d7caba39b6dc9198ed6089db1442ee8f47c3b5
atul <atul@DESKTOP-70D6P2M>
2019-12-25 15:12:45 +0530

    added application.properties

--- NewToGitHub/src/main/java/com/atul/configuration/HibernateConfiguration.java
+++ NewToGitHub/src/main/java/com/atul/configuration/HibernateConfiguration.java
@@ -1,5 +1,12 @@
 package com.atul.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"com.atul.configuration"})
 public class HibernateConfiguration {
 
 }
